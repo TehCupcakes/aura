3e144c44ece26466dc0a1478ff4dac4cd8c3d9a6
Michael Kirkpatrick <tehcupcakes@hotmail.com>
2014-07-31 18:01:34 -0400

    Changed skillUser to creature to comply with naming elsewhere.

--- src/ChannelServer/Skills/Life/FirstAidSkillHandler.cs
+++ src/ChannelServer/Skills/Life/FirstAidSkillHandler.cs
@@ -35,10 +35,10 @@ namespace Aura.Channel.Skills.FirstAid
 			Send.SkillStackSet(creature, skill.Info.Id, 1, 1);
 		}
 
-		public override TargetResult Use(Creature skillUser, Skill skill, long targetEntityId)
+		public override TargetResult Use(Creature creature, Skill skill, long targetEntityId)
 		{
 			// Check target
-			var target = skillUser.Region.GetCreature(targetEntityId);
+			var target = creature.Region.GetCreature(targetEntityId);
 
 			// Can't heal a dead man (UNTESTED) or a non-friendly creature
 			if (target == null || target.Has(CreatureStates.Dead) || (!target.Has(CreatureStates.GoodNpc) && !(target.IsPlayer)))
@@ -46,37 +46,37 @@ namespace Aura.Channel.Skills.FirstAid
 
 			// Check range
 			var targetPosition = target.GetPosition();
-			if (!skillUser.GetPosition().InRange(targetPosition, skillUser.AttackRangeFor(target)))
+			if (!creature.GetPosition().InRange(targetPosition, creature.AttackRangeFor(target)))
 				return TargetResult.OutOfRange;
 
 			// Stop movement
-			skillUser.StopMove();
+			creature.StopMove();
 
 			// Determine which bandage to use
-			Item bandage = this.GetBandage(skillUser);
+			Item bandage = this.GetBandage(creature);
 
 			// Calculate injuries to be healed (% of LifeMax)
-			var heal = this.GetHeal(skillUser, skill, target);
+			var heal = this.GetHeal(skill, target);
 
 			// Update skill stack
-			Send.SkillStackUpdate(skillUser, skill.Info.Id, 0, 1, 0);
+			Send.SkillStackUpdate(creature, skill.Info.Id, 0, 1, 0);
 
 			// Heal wounds
 			// use bandage
 			target.Injuries -= heal;
 
-			Send.SkillUseFirstAid(skillUser, skill.Info.Id, targetEntityId);
+			Send.SkillUseFirstAid(creature, skill.Info.Id, targetEntityId);
 
 			return TargetResult.Okay;
 		}
 
-		protected Item GetBandage(Creature skillUser)
+		protected Item GetBandage(Creature creature)
 		{
 			// TODO: Get creature's finest bandage
 			return null;
 		}
 
-		protected int GetHeal(Creature skillUser, Skill skill, Creature target)
+		protected int GetHeal(Skill skill, Creature target)
 		{
 			// No wound healing effect for novice; just train skill
 			if (skill.Info.Rank == SkillRank.Novice)
